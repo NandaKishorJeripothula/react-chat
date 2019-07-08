42ab02847a9664f639107b40203fcf0c09073044
Nandu <j.kishor.bd@gmail.com>
2019-07-08 21:58:37 +0530

    Updates

---
+++ precache-manifest.b120a6d01ee8f24c45a2c8a85090e2f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010428659df7873e82398e8290191d08",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "1f105b2063e0bf0acaa6",
+    "url": "/react-chat/static/css/main.76ed828f.chunk.css"
+  },
+  {
+    "revision": "d7390b91cba68512b2ef",
+    "url": "/react-chat/static/js/2.89d2fbe8.chunk.js"
+  },
+  {
+    "revision": "1f105b2063e0bf0acaa6",
+    "url": "/react-chat/static/js/main.08027bd1.chunk.js"
+  },
+  {
+    "revision": "13110edec713851423b8",
+    "url": "/react-chat/static/js/runtime~main.731731e6.js"
+  }
+]);
